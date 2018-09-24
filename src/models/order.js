cc20df5a78b4255d30de0f2d42d78de35a35c801
Quentin <qburg@cutii.io>
2018-09-24 11:43:51 +0200

    update models

--- src/models/order.js
+++ src/models/order.js
@@ -6,10 +6,6 @@ export default (sequelize, DataTypes) => {
       type         : DataTypes.UUID,
       defaultValue : DataTypes.UUIDV1,
     },
-    // TODO : improve that
-    // orderNumber : {
-    //   type : DataTypes.INTEGER,
-    // },
     articleId : {
       type : DataTypes.INTEGER,
     },
